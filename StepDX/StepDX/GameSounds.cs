397744318ba02ad2992933f895f1291c65f29529
mmarinetti <marinett@msu.edu>
2014-04-23 14:04:02 -0400

    project and assets

---
+++ StepDX/StepDX/GameSounds.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+using Microsoft.DirectX;
+using Microsoft.DirectX.DirectSound;
+
+namespace StepDx
+{
+    public class GameSounds
+    {
+        private Device SoundDevice = null;
+
+        private SecondaryBuffer[] clank = new SecondaryBuffer[10];
+        int clankToUse = 0;
+
+        private SecondaryBuffer nah = null;
+        private SecondaryBuffer arms = null;
+
+        public GameSounds(Form form)
+        {
+            SoundDevice = new Device();
+            SoundDevice.SetCooperativeLevel(form, CooperativeLevel.Priority);
+
+            Load(ref nah, "../../Nah1.wav");
+            Load(ref arms, "../../arms.wav");
+
+            for (int i = 0; i < clank.Length; i++)
+                Load(ref clank[i], "../../clank.wav");
+        }
+
+        private void Load(ref SecondaryBuffer buffer, string filename)
+        {
+            try
+            {
+                buffer = new SecondaryBuffer(filename, SoundDevice);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Unable to load " + filename,
+                                "Danger, Will Robinson", MessageBoxButtons.OK);
+                buffer = null;
+            }
+        }
+
+        public void Clank()
+        {
+            clankToUse = (clankToUse + 1) % clank.Length;
+
+            if (clank[clankToUse] == null)
+                return;
+
+            if (!clank[clankToUse].Status.Playing)
+                clank[clankToUse].Play(0, BufferPlayFlags.Default);
+        }
+
+        public void Nah()
+        {
+            if (nah == null)
+                return;
+
+            if (!nah.Status.Playing)
+                nah.Play(0, BufferPlayFlags.Default);
+        }
+
+        public void Arms()
+        {
+            if (arms == null)
+                return;
+
+            if (!arms.Status.Playing)
+            {
+                arms.SetCurrentPosition(0);
+                arms.Play(0, BufferPlayFlags.Default);
+            }
+        }
+
+        public void ArmsEnd()
+        {
+            if (arms == null)
+                return;
+
+            if (arms.Status.Playing)
+                arms.Stop();
+        }
+
+    }
+}
---
+++ StepDX/StepDX/GameSprite.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using Microsoft.DirectX;
+using Microsoft.DirectX.Direct3D;
+
+namespace StepDX
+{
+    public class GameSprite : PolygonTextured
+    {
+        private Vector2 p = new Vector2(0, 0);  // Position
+        private Vector2 v = new Vector2(0, 0);  // Velocity
+        private Vector2 a = new Vector2(0, 0);  // Acceleration
+
+        public Vector2 P { set { p = value; } get { return p; } }
+        public Vector2 V { set { v = value; } get { return v; } }
+        public Vector2 A { set { a = value; } get { return a; } }
+
+        private Vector2 pSave;  // Position
+        private Vector2 vSave;  // Velocity
+        private Vector2 aSave;  // Acceleration
+
+        private float spriteTime = 0;
+        private float spriteRate = 6;   // 6 per second
+
+        public void SaveState()
+        {
+            pSave = p;
+            vSave = v;
+            aSave = a;
+        }
+
+        public void RestoreState()
+        {
+            p = pSave;
+            v = vSave;
+            a = aSave;
+        }
+
+        protected List<Vector2> verticesM = new List<Vector2>();  // The vertices
+
+        public override List<Vector2> Vertices { get { return verticesM; } }
+
+        public override void Advance(float dt)
+        {
+            // Euler steps
+            v.X += a.X * dt;
+            v.Y += a.Y * dt;
+            p.X += 2.5f * dt;
+            p.Y += v.Y * dt;
+
+            int spriteNum;
+
+            if (p.X < 0.4f)
+            {
+                p.X = 0.4f;
+            }
+            else if (p.X > 31.6f)
+            {
+                //p.X = 31.6f;
+            }
+
+            if (v.X == 0)
+            {
+                spriteNum = 5;
+                spriteTime = 0;
+            }
+            else
+            {
+                spriteTime += dt;
+                spriteNum = (int)(spriteTime * spriteRate) % 4;     // 4 images
+            }
+
+            // Create the texture vertices
+            textureC.Clear();
+            if (v.X >= 0)
+            {
+                textureC.Add(new Vector2(spriteNum * 0.125f, 1));
+                textureC.Add(new Vector2(spriteNum * 0.125f, 0));
+                textureC.Add(new Vector2((spriteNum + 1) * 0.125f, 0));
+                textureC.Add(new Vector2((spriteNum + 1) * 0.125f, 1));
+            }
+            else
+            {
+                // If moving in the negative direction, we draw our sprite 
+                // as a mirror image.
+                textureC.Add(new Vector2((spriteNum + 1) * 0.125f, 1));
+                textureC.Add(new Vector2((spriteNum + 1) * 0.125f, 0));
+                textureC.Add(new Vector2(spriteNum * 0.125f, 0));
+                textureC.Add(new Vector2(spriteNum * 0.125f, 1));
+            }
+
+            if (v.Y != 0)
+            {
+                spriteNum = 7;
+            }
+
+            // Move the vertices
+            verticesM.Clear();
+            foreach (Vector2 x in verticesB)
+            {
+                verticesM.Add(new Vector2(x.X + p.X, x.Y + p.Y));
+            }
+        }
+    }
+}
